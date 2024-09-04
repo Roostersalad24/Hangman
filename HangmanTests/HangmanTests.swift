f8fb5382d72024988e2a1cf3d9087c7553f839a2
Roostersalad24 <114892410+Roostersalad24@users.noreply.github.com>
2024-09-03 23:02:46 -0500

    Initial Commit

---
+++ HangmanTests/HangmanTests.swift
@@ -0,0 +1,17 @@
+//
+//  HangmanTests.swift
+//  HangmanTests
+//
+//  Created by Andrew Johnson on 8/24/24.
+//
+
+import Testing
+@testable import Hangman
+
+struct HangmanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
